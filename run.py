b62f76bd0e5dc6bf292d393235a3565d0354110c
Marcian225 <Marcin.w.225@gmail.com>
2026-03-30 16:43:48 +0200

    Implement JSONL  reading and parallel filtering (rayon)

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
 import log_cruncher
 
-processed_logs = log_cruncher.process_logs_csv("test_logs.csv")
 
-print(processed_logs)
+
+
+def main():
+    # processed_logs = log_cruncher.process_logs_csv("test_logs.csv")
+    # processed_logs = log_cruncher.get_error_logs("test_logs.csv")
+    processed_logs = log_cruncher.process_logs_json("test_logs.jsonl")
+    
+    for log in processed_logs:  
+        print(log)
+    
+    
+    print("first log level: ", processed_logs[0].level)
+
+
+main()
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 use pyo3::prelude::*;
 use serde::Deserialize;
+use rayon::prelude::*;
+use std::io::{BufRead, BufReader};
+use std::fs::File;
 
 
 /// A Python module implemented in Rust.
@@ -8,18 +11,23 @@ mod log_cruncher {
 
     use pyo3::{exceptions::PyValueError, prelude::*};
 
-    use crate::{LogEntry, parse_csv};
-
-    /// Formats the sum of two numbers as string.
-    #[pyfunction]
-    fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-        Ok((a + b).to_string())
-    }
-
+    use crate::{LogEntry, filter_errors, parse_csv, parse_json_lines};
 
     #[pyfunction]
     fn process_logs_csv(file_path: &str) -> PyResult<Vec<LogEntry>>{
         parse_csv(file_path).map_err(|e| PyValueError::new_err(e.to_string()))
+    }
+
+    #[pyfunction]
+    fn get_error_logs(file_path: &str) -> PyResult<Vec<LogEntry>>{
+        let logs = parse_csv(file_path).map_err(|e| PyValueError::new_err(e.to_string()))?;
+        Ok(filter_errors(logs))
+    }
+
+    #[pyfunction]
+
+    fn process_logs_json(file_path: &str) -> PyResult<Vec<LogEntry>>{
+        parse_json_lines(file_path).map_err(|e| PyValueError::new_err(e.to_string()))
     }
 
 }
@@ -27,23 +35,58 @@ mod log_cruncher {
 #[derive(Deserialize, Debug, Clone)]
 #[pyclass]
 struct LogEntry {
+    #[pyo3(get)]
     timestamp: String,
+    #[pyo3(get)]
     level: String,
+    #[pyo3(get)]
     message: String,
+    #[pyo3(get)]
     user_id: u32,
 }
 
+#[pymethods]
+impl LogEntry {
+    fn __repr__(&self) -> String {
+        format!("LogEntry {{ timestamp: {}, level: {}, message: {}, user_id: {} }}", self.timestamp, self.level, self.message, self.user_id)
+    }
+}
+
+
+// Function to parse CSV file and return a vector of LogEntry structs
 fn parse_csv(file_path: &str) -> Result<Vec<LogEntry>, csv::Error>{
     let mut reader = csv::Reader::from_path(file_path)?;
-
     let mut output: Vec<LogEntry> = vec![];
-
-
+    
     for result in reader.deserialize(){
         let record = result?;
         output.push(record);
     }
-
+    
     return Ok(output);
+}
 
+// function to filter log entries for errors parallel using rayon into_par_iter()
+// example function to extract specific log entries 
+fn filter_errors(logs: Vec<LogEntry>) -> Vec<LogEntry> {
+    logs.into_par_iter().filter(|log| log.level =="ERROR").collect()
+}
+
+
+
+fn parse_json_lines(file_path: &str) -> Result<Vec<LogEntry>, std::io::Error> {
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+    let mut output: Vec<LogEntry> = vec![];
+
+    for line in reader.lines(){
+        let line_str = line?;
+        
+        match serde_json::from_str::<LogEntry>(&line_str) {
+            Ok(record) => output.push(record),
+            Err(err) => eprintln!("Failed to parse line: {}. Error: {:?}", line_str, err),
+        }
+    }
+
+    return Ok(output)
 }
